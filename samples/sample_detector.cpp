7cdc80afe0402720644974d1ca3512910a665d8b
zoom1539 <zoom1539@foxmail.com>
2020-11-06 21:50:15 +0800

    update

--- samples/sample_detector.cpp
+++ samples/sample_detector.cpp
@@ -24,8 +24,8 @@ int main()
 
 	Config config_v4;
 	config_v4.net_type = YOLOV4;
-	config_v4.file_model_cfg = "../configs/yolov4/yolov4.cfg";
-	config_v4.file_model_weights = "../configs/yolov4/yolov4.weights";
+	config_v4.file_model_cfg = "D:/yolo-tensorrt/configs/yolov4/yolov4.cfg";
+	config_v4.file_model_weights = "D:/yolo-tensorrt/configs/yolov4/yolov4.weights";
 	config_v4.inference_precison = TRT_FP32;
 
 	Config config_v4_tiny;
@@ -44,20 +44,20 @@ int main()
 	config_v5.inference_precison = TRT_FP32;
 	//config_v5.n_max_batch = 2;
 
-	cv::Mat image0 = cv::imread("../configs/person.jpg", cv::IMREAD_UNCHANGED);
-	cv::Mat image1 = cv::imread("../configs/dog.jpg", cv::IMREAD_UNCHANGED);
+	cv::Mat image0 = cv::imread("D:/yolo-tensorrt/configs/person.jpg", cv::IMREAD_UNCHANGED);
+	cv::Mat image1 = cv::imread("D:/yolo-tensorrt/configs/dog.jpg", cv::IMREAD_UNCHANGED);
 	std::unique_ptr<Detector> detector(new Detector());
 	detector->init(config_v4);
 	std::vector<BatchResult> batch_res;
 	Timer timer;
-	for (;;)
+	for (; ;)
 	{
 		//prepare batch data
 		std::vector<cv::Mat> batch_img;
 		cv::Mat temp0 = image0.clone();
 		cv::Mat temp1 = image1.clone();
 		batch_img.push_back(temp0);
-		batch_img.push_back(temp1);
+		//batch_img.push_back(temp1);
 
 		//detect
 		timer.reset();
@@ -79,4 +79,5 @@ int main()
 		}
 		cv::waitKey(10);
 	}
+	std::cin.get();
 }
